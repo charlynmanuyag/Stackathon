2f08e098661a4059578e86db23a371d8cd117399
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-04-26 14:45:05 -0400

    added bottom tab bar

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { Location, Permissions, MapView } from 'expo';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Text, View, Button, Header } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import ResultsScreen from './Results';
-import BarContainer from './BarContainer';
 import styles from './Style.js';
+import BoardScreen from './BoardScreen';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 class App extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
+    console.log('NAVIGATE', this.props);
     return (
       <View style={styles.container}>
         <Text>Welcome To Crawl!</Text>
@@ -22,12 +24,20 @@ class App extends React.Component {
   }
 }
 
-const MainNavigator = createStackNavigator({
-  App: { screen: App },
-  Results: { screen: ResultsScreen },
-  Bars: { screen: BarContainer },
-});
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: { screen: App },
+    Results: { screen: ResultsScreen },
+    Board: { screen: BoardScreen },
+  },
+  {
+    initialRouteName: 'Home',
+    activeColor: '#f0edf6',
+    inactiveColor: '#3e2465',
+    barStyle: { backgroundColor: '#694fad' },
+  }
+);
 
-const Apps = createAppContainer(MainNavigator);
+const AppContainer = createAppContainer(TabNavigator);
 
-export default Apps;
+export default AppContainer;
